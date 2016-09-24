b6d3d3d6ce9be58d7382f57af1bc2efc916db2b0
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-24 17:45:34 +0300

    Added icon plus and menu item Add post

--- app/src/main/java/ru/solandme/simpleblog/MainActivity.java
+++ app/src/main/java/ru/solandme/simpleblog/MainActivity.java
@@ -1,7 +1,8 @@
 package ru.solandme.simpleblog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
