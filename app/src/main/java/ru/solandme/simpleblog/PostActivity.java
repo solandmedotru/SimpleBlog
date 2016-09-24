a022c3472af8d6b2c90404d05f85794c4ebbdd41
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-24 19:31:54 +0300

    Created action on image click

--- app/src/main/java/ru/solandme/simpleblog/PostActivity.java
+++ app/src/main/java/ru/solandme/simpleblog/PostActivity.java
@@ -1,13 +1,43 @@
 package ru.solandme.simpleblog;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton selectImage;
+    private static final int GALLARY_REQUEST = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        selectImage = (ImageButton) findViewById(R.id.imageSelect);
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galaryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galaryIntent.setType("image/*");
+                startActivityForResult(galaryIntent, GALLARY_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLARY_REQUEST && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            selectImage.setImageURI(imageUri);
+
+        }
     }
 }
